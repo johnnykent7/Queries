3fa2d4ec06abf6f8ea2884bbe204a736565bebf8
Ion <ionglavan@mail.ru>
2018-07-14 15:13:35 +0300

    Add project files.

---
+++ Queries/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Linq;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new PlutoContext();
+            var querry = from c in context.Courses
+                         group c by c.AuthorId
+                         into g
+                         select g;
+
+            foreach(var ai in querry)
+            {
+                System.Console.WriteLine(ai.Key);
+                foreach(var gi in ai)
+                {
+                    System.Console.WriteLine("curs name:"+gi.Name+"description name:"+gi.Description);
+                }
+            }
+        }
+    }
+}
